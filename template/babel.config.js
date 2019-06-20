99b38e50611f9b3125e1bc042772230ffc9ffc6f
bugszhou <bugszhou@outlook.com>
2019-06-20 12:06:03 +0800

    feat(babel): Add Ant Design Vue babel plugin

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,5 +1,25 @@
+
+{{#antd}}
+/**
+ * antd按需加载
+ * @returns {*[]}
+ */
+function antdBabel() {
+  return [
+    "import",
+    { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
+  ];
+}
+{{/antd}}
+
 module.exports = {
   presets: [
     '@vue/app',
+  ],
+  plugins: [
+    {{#antd}}
+    // antd按需加载
+    antdBabel(),
+    {{/antd}}
   ],
 };
