0b390a9d89a08722cbf9a22fa17b4900aad03004
bugszhou <bugszhou@outlook.com>
2019-06-20 10:12:36 +0800

    feat: Initi

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+        // not require constant expressions in conditions
+        'no-constant-condition': 0,
+        // allow multiline strings
+        'no-multi-str': 0,
+        // enforce 4 space indent
+        'indent': ["error", 2],
+        // allow variable decalared separately in functions
+        'one-var': 0,
+        // allow either backticks, double or single quotes
+        'quotes': 0,
+        // enforce no sapce before function parent
+        'space-before-function-paren': ['error', 'never'],
+        // not require camlecase
+        'camelcase': 0,
+        // not require newline at the end of files
+        'eol-last': 0,
+        // disallow semi in line end
+        'semi': ["error", "always"],
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
---
+++ src/views/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>初始化完成</div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+};
+</script>
+
+<style lang="scss">
+
+</style>
