e22c38cbaabf1c7ef794175325c693cb948ef635
bugszhou <bugszhou@outlook.com>
2019-06-20 12:21:19 +0800

    feat(pkg): Add less less-loader

--- template/src/main.js
+++ template/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+{{#antd}}
+import './antd';
+{{/antd}}
 
 Vue.config.productionTip = false;
 
